d71002357a19286dcbd6ce0fc4bfc65b8bf82e5a
Abderrahman Elkhalil <aelkhali@e2r1p8.1337.ma>
2022-11-02 10:31:20 +0100

    fresh and new workspace for th

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelkhali <aelkhali@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/02 09:51:11 by aelkhali          #+#    #+#             */
+/*   Updated: 2022/11/02 09:55:23 by aelkhali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "stdlib.h"
+# include "unistd.h"
+
+char	*get_next_line(int fd);
+
+
+#endif
