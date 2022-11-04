592cdb8b288158fdf6761f86ac671121f17cd9c7
Abderrahman Elkhalil <aelkhali@e2r1p8.1337.ma>
2022-11-04 10:20:04 +0100

    first try

--- get_next_line.c
+++ get_next_line.c
@@ -6,57 +6,64 @@
 /*   By: aelkhali <aelkhali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 09:56:13 by aelkhali          #+#    #+#             */
-/*   Updated: 2022/11/03 13:14:18 by aelkhali         ###   ########.fr       */
+/*   Updated: 2022/11/04 10:18:44 by aelkhali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strchr(char *s, int c)
+char	*re_n_store(int fd)
 {
-	char	cas_c;
-	int		i;
+	int			i_read;
+	char		*buf;
+	char static	*save;
 
-	if (!s)
-		return (0);
-	i = 0;
-	cas_c = (char )c;
-	while (s[i] && s[i] != cas_c)
-		i++;
-	if (s[i] == '\0' && cas_c != s[i])
+	i_read = 1;
+	save = NULL;
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
 		return (NULL);
-	return ((char *)&s[i]);
+	while (!ft_strchr(save, '\n') && i_read != 0)
+	{
+		i_read = (int )read(fd, buf, BUFFER_SIZE);
+		if (i_read == -1)
+		{
+			free(buf);
+			free(save);
+			return (NULL);
+		}
+		buf[i_read] = '\0';
+		save = ft_strjoin(save, buf);
+	}
+	if (!ft_strchr(save, '\n'))
+		return (save);
+	free(buf);
+	return (save);
+}
+
+char	*extract_line(char *str)
+{
+	char	*extracted_line;
+	char	*tmp;
+	size_t	line_len;
+
+	line_len = ft_strlen(str) - ft_strlen(ft_strchr(str, '\n'));
+	extracted_line = malloc(line_len + 1);
+	if (!extracted_line)
+		return (NULL);
+	ft_strlcpy(extracted_line, str, line_len + 1);
+	tmp = ft_substr(str, line_len, ft_strlen(ft_strchr(str, '\n')));
+	free(str);
+	str = ft_strdup(tmp);
+	return (extracted_line);
 }
 
 char	*get_next_line(int fd)
 {
-	char 		*str_reader;
-	static char	*storage;
-	char		*line;
-	int			i;
-	int			i_readed;
+	char	*line;
 
-	str_reader = NULL;
-	line = NULL;
-	i_readed = 1;
-	storage = NULL;
-	i = 0;
-	if (fd < 0 || BUFFER_SIZE <= 0 || !read(fd, str_reader, BUFFER_SIZE))
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	str_reader = malloc(BUFFER_SIZE + 1);
-	if (!str_reader)
-		return (NULL);
-	while (i_readed >= 0 && !ft_strchr(storage, '\n'))
-	{
-		i_readed = read(fd, str_reader, BUFFER_SIZE);
-		str_reader[i_readed] = '\0';
-		storage = ft_strjoin(str_reader, storage);
-		if (!ft_strchr(str_reader, '\n'))
-			return(str_reader);
-	}
-	while (storage[i] && storage[i] != '\n')
-		i++;
-	line = ft_substr(storage, 0, i + 1);
-	storage = ft_substr(storage , i, ft_strlen(ft_strchr(storage,'\n')));
+	line = extract_line(re_n_store(fd));
 	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,23 +6,106 @@
 /*   By: aelkhali <aelkhali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 07:44:24 by aelkhali          #+#    #+#             */
-/*   Updated: 2022/11/03 13:11:34 by aelkhali         ###   ########.fr       */
+/*   Updated: 2022/11/04 08:59:21 by aelkhali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen( char *s)
+size_t	ft_strlen(char *s)
 {
 	size_t	len;
 
 	len = 0;
+	if (!s)
+		return (0);
 	while (*s++)
 		len++;
 	return (len);
 }
 
-char	*ft_strdup( char *s1)
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize)
+{
+	size_t	src_length;
+
+	src_length = ft_strlen(src);
+	if (dstsize > 0)
+	{	
+		while (*src && --dstsize)
+			*dst++ = *src++;
+		*dst = '\0';
+	}
+	return (src_length);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new_str;
+	char	*recall;
+
+	if (!s1)
+	{
+		s1 = malloc (1);
+		if (!s1)
+			return (NULL);
+		*s1 = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	new_str = malloc ((ft_strlen(s1) + ft_strlen(s2)) + 1);
+	recall = new_str;
+	if (!new_str)
+		return (NULL);
+	while (*s1)
+		*new_str++ = *s1++;
+	while (*s2)
+		*new_str++ = *s2++;
+	*new_str = '\0';
+	return (recall);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	char	cas_c;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	cas_c = (char )c;
+	while (s[i] && s[i] != cas_c)
+		i++;
+	if (s[i] == '\0' && cas_c != s[i])
+		return (NULL);
+	return ((char *)&s[i]);
+}
+
+char	*ft_substr(char  *s, unsigned int start, size_t len)
+{
+	size_t		i;
+	char		*sub_str;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		s = malloc(2);
+		if (!s)
+			return (NULL);
+		*s = '\0';
+		return (s);
+	}
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	sub_str = malloc (len + 1);
+	if (!sub_str)
+		return (NULL);
+	ft_strlcpy(sub_str, s + start, len + 1);
+	return (sub_str);
+}
+
+char	*ft_strdup(char *s1)
 {
 	char	*copied_str;
 	size_t	lentgh;
@@ -40,61 +123,4 @@ char	*ft_strdup( char *s1)
 	}
 	copied_str[i] = '\0';
 	return (copied_str);
-}
-
-size_t	ft_strlcpy(char *dst,  char *src, size_t dstsize)
-{
-	size_t	src_length;
-
-	src_length = ft_strlen(src);
-	if (dstsize > 0)
-	{
-		while (*src && --dstsize)
-			*dst++ = *src++;
-		*dst = '\0';
-	}
-	return (src_length);
-}
-
-char	*ft_strjoin(char *str_read, char *storage)
-{
-	char	*new_str;
-	char	*recall;
-
-	if (!str_read && !storage)
-		return (NULL);
-	if (!storage)
-		return(ft_strdup(str_read));
-	if (!str_read)
-		return (ft_strdup(storage));
-	new_str = malloc ((ft_strlen(str_read) + ft_strlen(storage)) + 1);
-	recall = new_str;
-	if (!new_str)
-		return (NULL);
-	while (*storage)
-		*new_str++ = *storage++;
-	while (*str_read)
-		*new_str++ = *str_read++;
-	*new_str = '\0';
-	free(str_read);
-	return (recall);
-}
-
-char	*ft_substr(char  *s, unsigned int start, size_t len)
-{
-	size_t		i;
-	char		*sub_str;
-
-	i = 0;
-	if (!s)
-		return (NULL);
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	if (len > ft_strlen(s) - start)
-		len = ft_strlen(s) - start;
-	sub_str = malloc (len + 1);
-	if (!sub_str)
-		return (NULL);
-	ft_strlcpy(sub_str, s + start, len + 1);
-	return (sub_str);
 }
